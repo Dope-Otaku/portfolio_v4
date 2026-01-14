3866b3fb40a554731617f34ad305fdc2b7b9c003
Souvik Halder <fakeyt909@gmail.com>
2026-01-14 14:50:16 +0530

    added an extra page for checking of the values

--- portfolio_v4/src/app/blog/page.js
+++ portfolio_v4/src/app/blog/page.js
@@ -1,5 +1,19 @@
 export default function page() {
+    const posts = [
+        { id: 1, title: "First Post", body: "This is the first post." },
+        { id: 2, title: "Second Post", body: "This is the second post." },
+    ];
   return (
-    <h1>Welcome to blog</h1>
+    <>
+        <h1>Welcome to blogs</h1>
+        {posts.map((post)=>{
+            return <div>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+            </div>;
+        })}
+    
+    </>
+    
   );
 }
