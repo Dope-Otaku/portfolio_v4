143477babe7e1526dfa189b6c81c81ce551e874f
Souvik Halder <fakeyt909@gmail.com>
2026-01-16 22:28:02 +0530

    now i have learned about page routing

--- portfolio_v4/src/app/api/page.js
+++ portfolio_v4/src/app/api/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function page(){
     return(
         <>
         <h1>Welcome to API Page</h1>
+        
         </>
     )
 }
--- portfolio_v4/src/app/page.js
+++ portfolio_v4/src/app/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
      <h1>Welcome to Home Page</h1>
+     <h2>
+            <Link href="/api">Go to Hello API Route</Link>
+        </h2>
     
     </>
   );
