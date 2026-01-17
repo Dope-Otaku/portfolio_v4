26ba44f184da55c6aa370898009adb3c3cabd20b
Souvik Halder <fakeyt909@gmail.com>
2026-01-17 11:36:45 +0530

    imported header independently

--- portfolio_v4/src/app/layout.js
+++ portfolio_v4/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
+import header from "./header/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {header()}
+        <main>{children}</main>
       </body>
     </html>
   );
