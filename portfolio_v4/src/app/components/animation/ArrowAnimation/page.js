b48ed21d4df147cd122b5882654d89f98c8cc5b2
Souvik Halder <fakeyt909@gmail.com>
2026-01-24 22:42:03 +0530

    added a small commit

--- portfolio_v4/src/app/components/animation/ArrowAnimation/page.js
+++ portfolio_v4/src/app/components/animation/ArrowAnimation/page.js
@@ -3,7 +3,9 @@ import React from 'react'
 const arrowAnimation = () => {
   return (
     <div>
-        <h1>Hi this is arrow animation page</h1>
+        <h1>Hi this is arrow animation page
+            <h2>and this is a sub heading</h2>
+        </h1>
     </div>
   )
 }
