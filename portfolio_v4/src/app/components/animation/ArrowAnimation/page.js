d1640ac959d6cecb1c5d31d99ab968ca2e3af6b2
Souvik Halder <fakeyt909@gmail.com>
2026-01-28 22:30:41 +0530

    another simple commit

--- portfolio_v4/src/app/components/animation/ArrowAnimation/page.js
+++ portfolio_v4/src/app/components/animation/ArrowAnimation/page.js
@@ -6,6 +6,7 @@ const arrowAnimation = () => {
         <h1>Hi this is arrow animation page
             <h2>and this is a sub heading</h2>
             <h2>today as well i will not work hard</h2>
+            <h2>today as well i will not work hard</h2>
         </h1>
     </div>
   )
